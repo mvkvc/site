c72e18704e14f2dd26e3ac8ec721b0c3d0a86fb5
Marko Vukovic <mvkvc.git@gmail.com>
2023-05-23 11:50:02 -0400

    Change to use Phoenix

--- config/config.exs
+++ config/config.exs
@@ -1,20 +1,61 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
 import Config
 
-config :still,
-  dev_layout: false,
-  pass_through_copy: ["assets/fontello"],
-  input: Path.join(Path.dirname(__DIR__), "priv/site"),
-  output: Path.join(Path.dirname(__DIR__), "_site")
+# Configures the endpoint
+config :site, SiteWeb.Endpoint,
+  url: [host: "localhost"],
+  render_errors: [view: SiteWeb.ErrorView, accepts: ~w(html json), layout: false],
+  pubsub_server: Site.PubSub,
+  live_view: [signing_salt: "wPl4Fm/J"]
 
+# Configures the mailer
+#
+# By default it uses the "Local" adapter which stores the emails
+# locally. You can see the emails in your browser, at "/dev/mailbox".
+#
+# For production it's recommended to configure a different adapter
+# at the `config/runtime.exs`.
+config :site, Site.Mailer, adapter: Swoosh.Adapters.Local
+
+# Swoosh API client is needed for adapters other than SMTP.
+config :swoosh, :api_client, false
+
+# Configure esbuild (the version is required)
+config :esbuild,
+  version: "0.14.29",
+  default: [
+    args:
+      ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
+    cd: Path.expand("../assets", __DIR__),
+    env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
+  ]
+
+# Configure TailwindCSS
 config :tailwind,
   version: "3.2.4",
   default: [
     args: ~w(
       --config=tailwind.config.js
-      --input=css/input.css
-      --output=css/output.css
+      --input=css/app.css
+      --output=../priv/static/assets/app.css
     ),
-    cd: Path.expand("../priv/site/assets", __DIR__)
+    cd: Path.expand("../assets", __DIR__)
   ]
 
-import_config("#{Mix.env()}.exs")
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{config_env()}.exs"
--- config/test.exs
+++ config/test.exs
@@ -1,1 +1,18 @@
 import Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :site, SiteWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "G/roNIeLlrFnC3+qmMLwxPeHd4X/ELIxPEbceo/NBo+LTFQ79kNDGOhvyZF1wfgH",
+  server: false
+
+# In test we don't send emails.
+config :site, Site.Mailer,
+  adapter: Swoosh.Adapters.Test
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
