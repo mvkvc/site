a3cdfbd903010aa6d210a7cd935f7195cfe1ab01
Marko Vukovic <mvkvc.git@gmail.com>
2023-05-31 10:25:29 -0400

    Finally fixed build

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,10 @@ import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://mvk.vc',
   markdown: {
     syntaxHighlight: 'prism',
   },
   integrations: [svelte(), tailwind(), mdx()]
-  // vite: {
-  //   define: {
-  //     'process.env.GH_TOKEN': process.env.GH_TOKEN
-  //   }
-  // }
 });
