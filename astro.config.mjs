d0e318cf68d76c1fee0d787539f03711b0887d5e
Marko Vukovic <mvkvc.git@gmail.com>
2023-05-31 10:04:38 -0400

    Try fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,10 +9,5 @@ export default defineConfig({
   markdown: {
     syntaxHighlight: 'prism',
   },
-  integrations: [svelte(), tailwind(), mdx()],
-  vite: {
-    define: {
-      'process.env.GH_TOKEN': process.env.GH_TOKEN
-    }
-  }
+  integrations: [svelte(), tailwind(), mdx()]
 });
