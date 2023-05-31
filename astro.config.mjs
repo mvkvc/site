d9bf65218da83513fe27bdaabc931de3458449d7
Marko Vukovic <mvkvc.git@gmail.com>
2023-05-30 20:32:03 -0400

    Add Prism syntax highlighting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind()]
+  markdown: {
+    syntaxHighlight: 'prism',
+  },
+  integrations: [svelte(), tailwind(), mdx()]
 });
